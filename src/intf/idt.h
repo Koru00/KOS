3210118a90a24395a6a64f9eeeed63752956467d
Koru <git@koru.com>
2025-05-22 14:46:30 +0200

    wip irq timer, struct done

--- src/intf/idt.h
+++ src/intf/idt.h
@@ -16,6 +16,18 @@
 } __attribute__((packed));
 
 
+struct interrupt_descriptor
+{
+    uint16_t address_low;
+    uint16_t selector;
+    uint8_t ist;
+    uint8_t flags;
+    uint16_t address_mid;
+    uint32_t address_high;
+    uint32_t reserved;
+} __attribute__((packed));
+
+
 
     struct IDTPointer {
     uint16_t limit;
--- src/intf/isr.h
+++ src/intf/isr.h
@@ -9,6 +9,35 @@ typedef struct registers {
     uint32_t eip, cs, eflags;
 } registers_t;
 
+struct cpu_status_t {
+
+    uint64_t rax;
+    uint64_t rbx;
+    uint64_t rcx;
+    uint64_t rdx;
+    uint64_t rbp;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+
+    uint64_t vector_number;
+    uint64_t error_code;
+
+    uint64_t iret_rip;
+    uint64_t iret_cs;
+    uint64_t iret_flags;
+    uint64_t iret_rsp;
+    uint64_t iret_ss;
+
+};
+
 
 typedef void (*isr_t)(registers_t *);
 
